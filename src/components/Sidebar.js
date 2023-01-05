35440ebc1c9792fed904890d4fcdacbbc530b5a3
agouliel <agouliel@gmail.com>
2023-01-05 16:34:46 +0200

    highlight active links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,19 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import {LinkContainer} from 'react-router-bootstrap';
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <Navbar sticky="top" className="flex-column Sidebar">
       <Nav.Item>
         <LinkContainer to="/">
-        <Nav.Link>Feed</Nav.Link>
+        <Nav.Link as={NavLink} to="/">Feed</Nav.Link>
         </LinkContainer>
       </Nav.Item>
       <Nav.Item>
         <LinkContainer to="/explore">
-        <Nav.Link>Explore</Nav.Link>
+        <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
         </LinkContainer>
       </Nav.Item>
     </Navbar>
