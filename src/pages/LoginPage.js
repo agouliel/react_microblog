0ae90926d48a58e93645200af36a63c2c7fcb42d
agouliel <agouliel@gmail.com>
2023-01-07 10:58:38 +0200

    added registration ch7.8

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import Body from '../components/Body';
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
     return (
         <Body sidebar>
             <h1>Login form</h1>
             <p>TODO</p>
+            <p><Link to="/register">Register here</Link>!</p>
         </Body>
     );
 }
